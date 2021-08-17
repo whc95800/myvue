341ad2f50db433f9abd4dfc68a45f6bba0bc0f78
wanghuachen <150970520@qq.com>
2021-08-17 10:11:33 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <div id="root">
-
+  <div id="game">
+    <h1>The Game</h1>
+    <Turns/>
+    <WinResult/>
+    <Field/>
   </div>
 </template>
 
 <script>
+import Turns from "@/components/Turns";
+import WinResult from "@/components/WinResult";
+import Field from "@/components/Field";
 
+export default {
+  name: 'App',
+  components: {Turns,WinResult,Field},
+}
 
 </script>
 
 <style scoped>
-
-
+#game {
+  width: 640px;
+  margin: 0 auto;
+}
+h1 {
+  text-align: center;
+}
 </style>
---
+++ src/components/Field.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="field">
+    <table id="field-table">{{}}</table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Field",
+  addField(){
+    const ROWS=15;
+    const COLUMNS=15;
+    const table = document.getElementById("field-table");
+    for (let y = 0; y <ROWS; y++) {
+      const row = document.createElement("tr");
+      for (let x = 0; x < COLUMNS; x++) {
+        const box = document.createElement("td");
+        const chips = document.createElement("div");
+        const lastRow = document.createElement("div");
+        const lastColumn = document.createElement("div");
+        const lastRowColumn = document.createElement("div");
+        box.appendChild(chips);
+        row.appendChild(box);
+        if (x === 14){
+          lastRow.className="chip last-in-row";
+          box.appendChild(lastRow);
+        }
+        if (x === 14&&y === 14){
+          lastRowColumn.className="chip last-in-column last-in-row";
+          box.appendChild(lastRowColumn);
+        }
+        if (y === 14){
+          lastColumn.className="chip last-in-column";
+          box.appendChild(lastColumn);
+        }
+
+        chips.className = "chip";
+
+        chips.innerHTML="0";
+        lastRow.innerHTML="0";
+        lastColumn.innerHTML="0";
+        lastRowColumn.innerHTML="0";
+      }
+      table.appendChild(row);
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.field {
+  width: 640px;
+  height: 640px;
+  background-color: #966f54;
+  padding: 0.05px;
+}
+
+#field-table {
+  border: 2px solid #4d3420;
+  margin: 40px;
+  border-collapse: collapse;
+  width: 560px;
+  height: 560px;
+}
+
+#field-table td {
+  border: 2px solid #4d3420;
+  border-spacing: 0;
+  padding: 0;
+  position: relative;
+}
+
+.chip {
+  border-radius: 17px;
+  height: 34px;
+  width: 34px;
+  position: absolute;
+  left: -18px;
+  top: -18px;
+  transition: background-color 0.2s ease;
+}
+
+.chip:hover {
+  background-color: rgb(255, 255, 255, 0.5);
+}
+
+.chip.last-in-row {
+  right: -18px;
+  left: inherit;
+}
+
+.chip.last-in-column {
+  bottom: -18px;
+  top: inherit;
+}
+
+.chip.black {
+  background: radial-gradient(
+    farthest-side at 34px 34px,
+    #ffffff 0%,
+    #000000 70%
+  );
+  box-shadow: -3px -3px 10px black;
+}
+
+.chip.white {
+  background: radial-gradient(
+    farthest-side at 34px 34px,
+    #ffffff 0%,
+    #d0cfcd 70%
+  );
+  box-shadow: -3px -3px 10px black;
+}
+</style>
