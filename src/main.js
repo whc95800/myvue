e4aefd211b6cd114b4d06074f2815833f2874f40
wanghuachen <150970520@qq.com>
2021-09-07 12:11:53 +0900

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vueResource from 'vue-resource'
 import Vuex from 'vuex'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(vueResource)
@@ -10,6 +11,7 @@ Vue.use(Vuex)
 new Vue({
   el:'#app',
   render: h => h(App),
+  store,
   beforeCreate() {
     Vue.prototype.$bus=this
   }
